12eee1163380714d57c347add9f0be38c491cd6a
TUAN.NH2375 <gialuong12352@gmail.com>
2018-11-13 14:08:29 +0700

    concac

--- app/src/main/java/com/example/tuannh2375/smsspam/MainActivity.java
+++ app/src/main/java/com/example/tuannh2375/smsspam/MainActivity.java
@@ -68,65 +68,23 @@ public class MainActivity extends AppCompatActivity {
             ArrayList<String> arrayList = new ArrayList<>();
             arrayList.add( text.toString() );
 
-                try {
+
 
                     for (int i = 0; i < arrayList.size(); i++) {
-                        Thread.sleep( 10000 );
-                        SmsManager smsManager = SmsManager.getDefault();
-                        smsManager.sendTextMessage( arrayList.get( i ), null, "abc", null, null );
+                        txt1.setText(arrayList.get( i ));
                     }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+
                 }
 
 
 
-            btn1.setOnClickListener( new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    sendSMSMessage();
-                }
-            } );
 
 
-        }
-    }
-    private void sendSMSMessage() {
-
-            phoneNo = txt1.getText().toString();
-            message = txt2.getText().toString();
-
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.SEND_SMS)
-                != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.SEND_SMS)) {
-            } else {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, MY_PERMISSIONS_REQUEST_SEND_SMS);
-            }
         }
     }
 
 
 
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    SmsManager smsManager = SmsManager.getDefault();
-                    smsManager.sendTextMessage(phoneNo, null, message, null, null);
-                    Toast.makeText(getApplicationContext(), "SMS sent.",
-                            Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(getApplicationContext(),
-                            "SMS faild, please try again.", Toast.LENGTH_LONG).show();
-                    return;
-                }
-            }
-        }
 
-    }
-}
+
